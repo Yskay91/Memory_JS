62b58a33ac46d395e4fc3453b445a0056c808e5f
Yskay91 <jessica.eisele@protonmail.com>
2023-08-31 14:41:36 +0200

    Mise à jour du code

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-/*
-    Je veux un tableau de x lignes et y colonnes
-*/
-
 let tableauJeu;
 let allCards = document.querySelectorAll(".card");
 let cptClickCurrent = 0;
-let dataImageShowed;
+let cardClickedId;
 
-allCards.forEach(card => {
+/* Ajoute l''événement de clique sur toutes les cartes */
+allCards.forEach((card) => {
     card.addEventListener("click", function () {
-        playGame(card)
+        clickOnCardEvent(card);
     });
-})
+});
 
-function playGame(card) {
+/* Cette fonction gère ce qui se passe 
+quand on clique sur une carte 
+*/
+function clickOnCardEvent(card) {
+    if (card.classList.contains("finded")) {
+        alert("Vous avez déjà appuyé sur cette carte, choisissez-en une autre");
+        return;
+    }
     cptClickCurrent++;
 
     if (cptClickCurrent == 1) {
         //premier click,  je cache les images trouvées avant
-        allCards.forEach(card => {
+        allCards.forEach((card) => {
             if (card.classList.contains("finded")) {
                 //carte cachée
             } else {
@@ -29,23 +33,29 @@ function playGame(card) {
         //J'affiche la carte que je viens de cliquer
         card.classList.remove("hidden");
         //je stocke la réponse derrière la carte
-        dataImageShowed = card.dataset.image;
-
+        cardClickedId = card.id;
     } else if (cptClickCurrent == 2) {
-        //deuxième click, je vérifie si l'image a été trouvée
-        card.classList.remove("hidden");
-        if (dataImageShowed == card.dataset.image) {
-            allCards.forEach(card => {
-                if (card.classList.contains("hidden")) {
-                    //carte cachée
-                } else {
-                    card.classList.add("finded");
-                }
-            });
-        }
+        //deuxième click, je vérifie si l'image cliquée est la même qu'au premier click
+        if (cardClickedId == card.id) {
+            //si oui j'annule le deuxième clique
+            cptClickCurrent = 1;
+            return;
+        } else {
+            card.classList.remove("hidden");
+            let cardClickedBefore = document.getElementById(cardClickedId);
+            if (cardClickedBefore.dataset.image == card.dataset.image) {
+                allCards.forEach((card) => {
+                    if (card.classList.contains("hidden")) {
+                        //carte cachée
+                    } else {
+                        card.classList.add("finded");
+                    }
+                });
+            }
 
-        cptClickCurrent = 0;
-        //On enlève les cartes sauvegardées
-        dataImageShowed = "";
+            cptClickCurrent = 0;
+            //On enlève les cartes sauvegardées
+            cardClickedId = "";
+        }
     }
 }
